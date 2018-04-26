8493834daf098d4518305828cbee6b9b4944ebd1
fasthro <chenchaoflying@outlook.com>
2018-04-26 11:27:37 +0800

    添加type view 添加view BaseMixin

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path
 from blog.views import MainView
 from blog.views import TagsView
+from blog.views import TypesView
 
 app_name = 'blog'
 urlpatterns = [
     re_path(r'^$', MainView.as_view(), name='home'),
     re_path('^tags/(?P<tag_id>\w+)/$', TagsView.as_view(), name='tags'),
+    re_path('^types/(?P<type_id>\w+)/$', TypesView.as_view(), name='types'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,60 @@
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
 from  blog_user.models import BUser
-from blog_article.models import Article, ArticleTag
+from blog_article.models import Article, ArticleTag, ArticleType
 from blog import conf
 
 
-# home view
-class MainView(ListView):
-    model = Article
-    template_name = "blog/index.html"
-    paginate_by = conf.MAIN_PAGE_NUM
-
+# base mixin
+class BaseMixin(object):
     def get_context_data(self, **kwargs):
-        context = super(MainView, self).get_context_data(**kwargs)
+        context = super(BaseMixin, self).get_context_data(**kwargs)
         tags = ArticleTag.objects.order_by('order')
         # 全局所有 tag 类型
         context['tags'] = tags
         # 导航 tags 对应的默认 tag 类型
         context['nav_tag'] = tags[0]
+
+        # 全局所有 type 类型
+        types = ArticleType.objects.order_by('order')
+        # 全局所有 tag 类型
+        context['types'] = types
+        # 导航 types 对应的默认 type 类型
+        context['nav_type'] = types[0]
+
         # 当前作者信息
         context['author'] = BUser.objects.get(is_author=True)
         return context
 
 
+# home view
+class MainView(BaseMixin, ListView):
+    model = Article
+    template_name = "blog/index.html"
+    paginate_by = conf.MAIN_PAGE_NUM
+
+
+# types view
+class TypesView(BaseMixin, ListView):
+    model = Article
+    template_name = 'blog/type.html'
+    paginate_by = conf.MAIN_PAGE_NUM
+
+    def get_queryset(self):
+        tid = self.kwargs.get('type_id', '')
+        at = ArticleType.objects.get(type_id=tid)
+        return Article.objects.all().filter(type_id=at.id)
+
+    def get_context_data(self, **kwargs):
+        context = super(TypesView, self).get_context_data(**kwargs)
+        # 当前选择的类型
+        tid = self.kwargs.get('type_id', '')
+        context['select_type'] = ArticleType.objects.get(type_id=tid)
+        return context
+
+
 # tags view
-class TagsView(ListView):
+class TagsView(BaseMixin, ListView):
     model = Article
     template_name = 'blog/tags.html'
     paginate_by = conf.MAIN_PAGE_NUM
@@ -33,11 +63,9 @@ class TagsView(ListView):
         tag = self.kwargs.get('tag_id', '')
         return Article.objects.all().filter(tags__regex=tag)
 
-    def get_context_data(self,**kwargs):
+    def get_context_data(self, **kwargs):
         context = super(TagsView, self).get_context_data(**kwargs)
-        tags = ArticleTag.objects.order_by('order')
-        # 全局所有 tag 类型
-        context['tags'] = tags
-        # 导航 tags 对应的默认 tag 类型
-        context['nav_tag'] = tags[0]
+        # 当前选择的标签
+        context['select_tag'] = self.kwargs.get('tag_id', '')
         return context
+
